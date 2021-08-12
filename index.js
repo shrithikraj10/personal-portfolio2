f51c1acc287074914bde73e911fe7f88ce6b47cb
Shrithik Raj <70435367+shrithikraj10@users.noreply.github.com>
2021-08-12 18:32:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var aboutme = document.querySelector("contactbtn");
+
+document.querySelector(".contactbtn").onclick = function () {
+location.href = "#contactme";
+};
+
+document.querySelector(".abtbtn").onclick = function () {
+location.href = "#about-title";
+};
+
+document.querySelector(".scroll1").onclick = function () {
+location.href = "#projects";
+};
+
+document.querySelector(".scroll2").onclick = function () {
+location.href = "#about-title";
+};
+
+document.querySelector(".scroll3").onclick = function () {
+location.href = "#contactme";
+};
